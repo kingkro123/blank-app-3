54aeee5ef118fb9b1a9421aac46a830899e4c8a4
kingkro123 <kingkro@gmail.com>
2024-11-02 20:34:38 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
+secret_token = "mysecret"
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+
 )
+
+
+st.write(
+    "below is an embedded streamlit app."
+
+
+)
+
+iframe_src = f"https://chatbot-ch4trmyyf6u.streamlit.app/?embedded=true&token={secret_token}"
+components.iframe(iframe_src)
